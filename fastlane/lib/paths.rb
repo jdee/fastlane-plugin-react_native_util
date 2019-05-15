9d1bc09d893bd97cd47c4eaa6676ff8c8c2a2475
Jimmy Dee <jgvdthree@gmail.com>
2019-05-14 17:51:02 -0700

    rubocop

--- fastlane/lib/paths.rb
+++ fastlane/lib/paths.rb
@@ -1,2 +1,2 @@
 UNCONVERTED_APP_PATH = File.expand_path '../../examples/UnconvertedApp', __dir__
-CONVERTED_APP_PATH   = File.expand_path '../../examples/ConvertedApp'  , __dir__
+CONVERTED_APP_PATH   = File.expand_path '../../examples/ConvertedApp', __dir__
